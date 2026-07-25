73943e707c3ff0fd91232f5f9d652e42e8802484
rafiach <rahmadfahrudi321@gmail.com>
2026-07-25 12:48:38 +0700

    update derail project

--- app/(root)/projects/[projectId]/page.tsx
+++ app/(root)/projects/[projectId]/page.tsx
@@ -18,7 +18,7 @@ interface ProjectPageProps {
   }>;
 }
 
-const githubUsername = "namanbarkiya";
+const githubUsername = "rafiach";
 
 export default async function Project({ params }: ProjectPageProps) {
   const { projectId } = await params;
@@ -73,14 +73,14 @@ export default async function Project({ params }: ProjectPageProps) {
           >
             <Image
               src={profileImg}
-              alt={"naman"}
+              alt={"rafi"}
               width={42}
               height={42}
               className="rounded-full bg-background"
             />
 
             <div className="flex-1 text-left leading-tight">
-              <p className="font-medium">{"Naman Barkiya"}</p>
+              <p className="font-medium">{"Rafi Achmad Fachrudi"}</p>
               <p className="text-[12px] text-muted-foreground">
                 @{siteConfig.username}
               </p>
